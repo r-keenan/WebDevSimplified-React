7287595ada1a24647d4968d6f5d6daecc1cb0ea3
Ross Keenan <ross.keenan@protonmail.com>
2021-06-17 11:30:32 -0500

    Added new ToDoList component and wired it in. Added in two buttons and a div.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import ToDoList from "./components/ToDoList";
+
 function App() {
-  return null;
+  return (
+    <>
+      <ToDoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+      <div>0 left to do</div>
+    </>
+  );
 }
 
 export default App;
